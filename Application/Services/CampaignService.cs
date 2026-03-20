69be4b6bf4d2e6213ffdf1f81f779ea858eaada1
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2026-03-20 13:49:36 -0300

    Finished Campaign Service

--- Application/Services/CampaignService.cs
+++ Application/Services/CampaignService.cs
@@ -1,4 +1,5 @@
 ﻿using RollTheDie.Domain.Interfaces;
+using RollTheDie.Domain.Entities;
 
 namespace RollTheDie.Application.Services
 {
@@ -6,8 +7,48 @@ namespace RollTheDie.Application.Services
     {
         private readonly ICampaign _campaign;
 
-        public CampaignService(ICampaign campaign) { 
+        public CampaignService(ICampaign campaign)
+        {
             _campaign = campaign;
+        }
+
+        public async Task Create(Campaign campaign)
+        {
+            if (campaign.Name.Length > 50)
+            {
+                throw new Exception("Nome da Campanha não pode ser maior que 50 caracteres");
+            }
+            await _campaign.Create(campaign);
+        }
+
+        public async Task<Campaign?> GetCampaign(Guid id)
+        {
+
+            if (_campaign == null)
+            {
+                throw new Exception("Campanha não existe");
+            }
+
+            return await _campaign.GetCampaigntById(id);
+        }
+
+        public async Task<List<Campaign>> GetAll()
+        {
+            if (_campaign == null)
+            {
+                throw new Exception("Nenhuma Campanha Encontrad");
+            }
+            return await _campaign.GetAll();
+        }
+
+        public async Task Delete(Guid id)
+        {
+            var campaign = await GetCampaign(id);
+            if (campaign == null)
+            {
+                throw new Exception("Campanha não existe");
+            }
+            await _campaign.Delete(id);
         }
     }
 }
--- Infrasctructure/Repository/CampaignRepository.cs
+++ Infrasctructure/Repository/CampaignRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.AspNetCore.Http.HttpResults;
 using RollTheDie.Domain.Entities;
 
 public class CampaignRepository : ICampaign
